e6f283521a3c54a0197fe463be90d873d1b0e0f0
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-08-14 12:42:01 +0100

    server fetch planet data to client

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,6 +1,6 @@
-const parse = require('csv-parse');
 const fs = require('fs');
-
+const path = require('path');
+const {parse} = require('csv-parse');
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
@@ -9,12 +9,10 @@ function isHabitablePlanet(planet) {
     && planet['koi_prad'] < 1.6;
 }
 
-//Create a promise
-
 
 function loadPlanetsData(){
     return new Promise((resolve, reject) => {
-      fs.createReadStream('kepler_data.csv')
+      fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
       .pipe(parse({
         comment: '#',
         columns: true,
@@ -41,5 +39,6 @@ function loadPlanetsData(){
 
 
   module.exports = {
+    loadPlanetsData,
     planets: habitablePlanets,
   }
