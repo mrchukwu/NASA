4e38a5f755a2979bfcd955ebbd10139862c69ce5
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-08-19 08:54:46 +0100

    wrtten test case for POST /launch pass

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -12,7 +12,34 @@ describe('Test GET /launches', () => {
 })
 
 describe('Test POST /launch', () => {
-    test('It should respond with 201 success', () => {})
+
+    const completeLaunchData = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-62 f',
+        launchDate: 'January 4, 2028'
+    };
+
+    const launchDataWithoutDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-62 f'
+    };
+
+    test('It should respond with 201 success', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send(completeLaunchData)
+        .expect(201)
+        .expect('Content-Type', /json/);
+
+
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+        expect(responseDate).toBe(requestDate);
+
+        expect(response.body).toMatchObject(launchDataWithoutDate);
+    })
 
     test('It should catch missing required properties', () => {});
 
